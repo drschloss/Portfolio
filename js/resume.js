95da389779ddd074ea7b2f1e04da9eab1d48fa2d
Dave Schloss <drschloss@gmail.com>
2015-03-18 13:07:38 -0400

    Added started JS for resume.  Restructured source paths.  Added jquery.

---
+++ js/resume.js
@@ -0,0 +1,29 @@
+function resume() {
+  var name;
+  var address;
+  var objective;
+  var skills;
+  var edu;
+
+  function initData() {
+
+  }
+
+  function connectSQL() {
+
+  }
+
+  function getName() {
+    return this.name;
+  }
+
+  function getAddress() {
+    return this.address;
+  }
+
+  $(document).ready(function() {
+    $('body').text('Blah blah');
+  });
+}
+
+resume();
