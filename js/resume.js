26742c56d263115cd73c41be51336971f3d91a86
David Schloss <drschloss@gmail.com>
2015-03-23 09:00:04 -0400

    Major content additions.  Must be run from system hosting MySQL DB

--- js/resume.js
+++ js/resume.js
@@ -1,29 +1,172 @@
 function resume() {
   var name;
+  var email;
   var address;
   var objective;
+  var exp;
+  var expDetail;
   var skills;
+  var skillType;
   var edu;
+  var self = this;
+  var queryResults;
+  this.initData = function() {
+    $.post('index.html', {query: 'name'}, function(data, status){
+      if(status === 'success'){
+        name = {
+          first: data[0].first,
+          mi: data[0].middle[0],
+          last: data[0].last
+        };
+        $('#name').html(printName(getName()));
+      } else {
+        console.log('Failure: '+ status);
+      }
+    });
+    email = "drschloss@gmail.com";
+    $.post('index.html', {query: 'address'}, function(data, status){
+      if(status === 'success'){
+        //alert(data.street)
+        address = {
+          streetNum: data[0].streetNum,
+          streetName: data[0].streetName,
+          aptNum: 11,
+          city: data[0].city,
+          state: data[0].state,
+          country: data[0].country,
+          zip: data[0].zip,
+          phone: data[0].phone
+        };
+        $('#footer').find('p').text(printAddress(getAddress()));
+        //$('#name').html(printName(getName()));
+      } else {
+        console.log('Failure: '+ status);
+      }
+    });
+    objective = "More than 6 years programming and application development experience with work on multiple contracts in vastly differing domains of technology.  Looking to make a major change in development environment to enable better flexibility and innovation.";
 
-  function initData() {
+    $.post('index.html', {query: 'experience'}, function(data, status){
+      if(status === 'success'){
+        exp = data;
+        $.post('index.html', {query: 'expDetail'}, function(data, status){
+          if(status === 'success'){
+            expDetail = data;
+          } else {
+            console.log('Failure: '+ status);
+          }
+          processExp();
+        });
+      } else {
+        console.log('Failure: '+ status);
+      }
+    });
 
-  }
+    $.post('index.html', {query: 'education'}, function(data, status){
+      if(status === 'success'){
+        edu = data;
+        processEdu();
+      } else {
+        console.log('Failure: '+ status);
+      }
+    });
+    $.post('index.html', {query: 'skillTypes'}, function(data, status){
+      if(status === 'success'){
+        skillType = data;
+      } else {
+        console.log('Failure: '+ status);
+      }
+    });
+    $.post('index.html', {query: 'skills'}, function(data, status){
+      if(status === 'success'){
+        skills = data;
+        $.post('index.html', {query: 'skillTypes'}, function(data, status){
+          if(status === 'success'){
+            skillType = data;
+          } else {
+            console.log('Failure: '+ status);
+          }
+          processSkills();
+        });
+      } else {
+        console.log('Failure: '+ status);
+      }
+    });
+  };
 
-  function connectSQL() {
+  this.getName = function() {
+    return name;
+  };
 
-  }
+  this.getEmail = function() {
+    return email;
+  };
 
-  function getName() {
-    return this.name;
-  }
+  this.printName = function(nameObj) {
+    var fullName = nameObj.first + " " + nameObj.mi + " " + nameObj.last;
 
-  function getAddress() {
-    return this.address;
-  }
+    return fullName;
+  };
+
+  this.getAddress = function() {
+    return address;
+  };
+
+  this.printAddress = function(addressObj) {
+    var fullAddress = addressObj.streetNum + " " + addressObj.streetName +
+      ", Apt. " + addressObj.aptNum + ", " + addressObj.city + ", " +
+      addressObj.state + " " + addressObj.zip + " " + address.country +
+      " | Tel: " + addressObj.phone;
+
+    return fullAddress;
+  };
+
+  this.processExp = function() {
+    $('.exp').each(function(i){
+      $(this).text(exp[i].position + ", " + exp[i].company +
+      " - " + exp[i].program + " (" + exp[i].startdate + " - " +
+      exp[i].enddate + ")");
+    });
+    //var j;
+    $('.expDetails').each(function(i){
+      for(var j=0; j<expDetail.length; j++){
+        if(expDetail[j].experiencekey === exp[i].key){
+          $(this).append('<li>' + expDetail[j].note + '</li>');
+        }
+      }
+    });
+  };
+
+  this.processSkills = function() {
+
+    $('.skillDetails').each(function(i){
+      $(this).text(skillType[i].category + ":");
+    });
+    $('.skill').each(function(i){
+      for(var j=0; j<skills.length; j++){
+        if(skills[j].category === skillType[i].category){
+          $(this).append('<li>' + skills[j].subcategory + ": " +
+          skills[j].skills + '</li>');
+        }
+      }
+    });
+  };
+
+  this.processEdu = function() {
+    $('.eduHead').each(function(i){
+      $(this).text(edu[i].univName + ", " + edu[i].city + ", " + edu[i].state +
+      " ("+edu[i].startyear+ " - " + edu[i].endyear + ")");
+    });
+    $('.eduDetails').each(function(i){
+      $(this).append('<li>' + edu[i].detail + '</li>');
+    });
+  };
 
   $(document).ready(function() {
-    var testDiv =
-    $('body').html('Blah blah');
+    self.initData();
+    $('a').first().attr('href', "mailto:drschloss@gmail.com");
+    $('#email').text(email);
+    $('#objective').next('p').text(objective);
+
   });
 }
 
---
+++ server/server.js
@@ -0,0 +1,109 @@
+var mysql = require('mysql');
+var path = require('path');
+var fs = require('fs');
+var queryResult;
+var query;
+var queryType;
+var skillTypeQuery = "Select * from skilltype";
+var skillQuery = "Select * from skill";
+var experienceQuery = "Select * from experience";
+var experienceDetailQuery = "Select * from experiencenote";
+var educationQuery = "Select * from education";
+var nameQuery = "Select first, middle, last from name";
+var addressQuery = "Select streetNum, streetName, city, state, country, zip,"+
+" phone from address";
+
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'xxxx',
+  password: 'xxxx',
+  database: 'xxxx',
+  port: '3306'
+});
+
+function server(args) {
+  var http = require('http');
+  var localFolder = args[2];
+  var ip = args[3];
+  var port = args[4];
+
+  this.connectSQL = function() {
+    connection.connect();
+  };
+
+  connectSQL();
+  http.createServer(function(req, res) {
+    var content = '',
+    fileName = path.basename(req.url);
+    console.log(req.url.toString());
+    if (req.method === 'POST') {
+      req.on('data', function(data) {
+        queryType = data.toString("utf8").split("=")[1];
+        resumeDBHandler(queryType);
+        console.log(query);
+        connection.query(query, function(err, rows, fields) {
+          if (!err) {
+            console.log(JSON.stringify(rows[0]));
+            res.writeHead(200, {
+              'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify(rows), 'utf8');
+            queryResult = rows;
+          } else
+            console.log('Error while performing DB Query.');
+        });
+
+      });
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      if (fileName === 'index.html' || fileName === '') {
+        fileName = 'index.html';
+        content = localFolder + fileName;
+        console.log(content);
+        fs.readFile(content, function(err, contents) {
+          if (!err) {
+            res.end(contents);
+          } else {
+            console.dir(err);
+          }
+        });
+      } else {
+        content = localFolder + req.url;
+        console.log(content);
+        fs.readFile(content, function(err, contents) {
+          if (!err) {
+            res.end(contents);
+          } else {
+            console.dir(err);
+          }
+        });
+      }
+    }
+
+  }).listen(Number(port), ip);
+}
+
+function resumeDBHandler(qType) {
+  if (qType === "experience") {
+    query = experienceQuery;
+  } else if (qType === "expDetail") {
+    query = experienceDetailQuery;
+  } else if (qType === "education") {
+    query = educationQuery;
+  } else if (qType === "address") {
+    query = addressQuery;
+  } else if (qType === "name") {
+    query = nameQuery;
+  } else if (qType === "skillTypes") {
+    query = skillTypeQuery;
+  } else if (qType === "skills") {
+    query = skillQuery;
+  }
+}
+
+function queryDB() {}
+
+server(process.argv);
+resumeDBHandler("name");
